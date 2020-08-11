fee5f4226958ed3c196cde0436ee7dc246d7da29
cheesama <cheehoon12@hanmail.net>
2020-08-11 19:08:47 +0900

    prepare inference server

--- morphine/inferencer.py
+++ morphine/inferencer.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 import dill
+import pycrfsuite
 
 app = FastAPI()
 is_ready = False
@@ -8,10 +9,19 @@ is_ready = False
 #load intent, entity model
 intent_model = None
 entity_model = None
-is_ready = True
+with open('./morphine_intent_model.svc', 'rb') as f:
+    intent_model = dill.load(f)
+    print ('intent model load success')
+
+entity_model = pycrfsuite.Tagger()
+entity_model.open('./morphine_entity_model.crfsuite')
+print ('entity model load success')
+
+if intent_model and entity_model:
+    is_ready = True
 
 #endpoints
-@app.get("/"):
+@app.get("/")
 async def health():
     if is_ready:
         output = {'code': 200}
@@ -19,11 +29,11 @@ async def health():
         output = {'code': 500}
     return output
 
-@app.post("/predict/intent"):
+@app.post("/predict/intent")
 async def predict_intent(text: str):
     return {}
 
-@app.post("/predict/entity"):
+@app.post("/predict/entity")
 async def predict_entity(text: str):
     return {}
 
